8f1df994885e6beadb11f147e609ee27102a3e56
hoanglv-0162 <hoangh2kq@gmail.com>
2021-12-17 13:25:04 +0700

    read workspace_users_<id>.txt : check user in WSP

--- workspace.c
+++ workspace.c
@@ -48,14 +48,44 @@ int isExistingWSP(char *input){
     return isExist;
 }
 
+int isInWSP(User user, int wsp_ID) {
+    int inWSP = 0;
+    char wspID[5];
+    sprintf(wspID, "%d", wsp_ID);
+    int userID;
+
+    FILE *f;
+    //r == read file text
+    char filename[MAX_LENGTH];
+    strcpy(filename, "db/");
+    strcat(filename, "workspace_users_");
+    strcat(filename, wspID);
+    if (!(f = fopen(filename, "r"))){
+        printf("\n File not found!! \n\n");
+    }
+    else{
+        while (!feof(f)){
+            fscanf(f, "%d\n", &userID);
+            if(user.ID == userID) inWSP = 1;
+        }
+    }
+    fclose(f);
+    return inWSP;
+}
+
 void signIn();
 void signUp();
 
-// int main(){
-//     WorkSpace *wspList = updateListWSP();
-//     WorkSpace *p = headWSP;
-//     while(p != NULL) {
-//         printf("%d --- %s --- %d\n", p->ID, p->name, p->host->ID);
-//         p = p->next;
-//     }
-// }
+int main(){
+    WorkSpace *wspList = updateListWSP();
+    WorkSpace *p = headWSP;
+    while(p != NULL) {
+        printf("%d --- %s --- %d\n", p->ID, p->name, p->host->ID);
+        p = p->next;
+    }
+    User x = *headUser;
+    printf("%s\n", x.name);
+    if(isInWSP(x, 1)) {
+        printf("%s is in WSP!\n", x.name);
+    }
+}
